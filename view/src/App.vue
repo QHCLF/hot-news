7f782f5a9117c72c4bb56e2f69e77ae3215cccbd
qhclf <3056149117@qq.com>
2019-12-07 22:43:18 +0800

    add more

---
+++ view/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Nav></Nav>
+    <img alt="Vue logo" src="./assets/logo.png">
+  </div>
+</template>
+
+<script>
+import Nav from './components/Nav';
+export default {
+  name: 'app',
+  components: { Nav }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  font-size: 16px;
+  color: #2c3e50;
+  padding: 5rem 10rem;
+}
+</style>
---
+++ view/src/components/Nav.vue
@@ -0,0 +1,74 @@
+<template>
+    <nav class="nav_contain">
+        <div class="nav_section home">
+            <img class="home_img" src="#">
+            <a href= '/home' >首页</a>
+        </div>
+        <div class="nav_section" v-for="(item, index) in navData"  :key="index">
+            <a href= item.href >{{item.text}}</a>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Nav',
+    data(){
+        return{
+            navData: [
+                {
+                    href: '#',
+                    text: '首页'
+                },
+                {
+                    href: '#',
+                    text: '国内'
+                },
+                {
+                    href: '#',
+                    text: '国际',
+                },
+                {
+                    href: '#',
+                    text: '励志',
+                },
+                {
+                    href: '#',
+                    text: '学习',
+                },
+                {
+                    href: '#',
+                    text: '娱乐',
+                },
+                {
+                    href: '#',
+                    text: '体育',
+                },
+                {
+                    href: '#',
+                    text: '科技',
+                },
+
+            ]
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.nav_contain{
+    display: flex;
+    justify-content: space-between;
+}
+
+.nav_section{
+    flex: 1;
+    padding: 0.5rem;
+    background-color: pink;
+}
+
+.nav_section a{
+    text-decoration: none;
+}
+</style>
